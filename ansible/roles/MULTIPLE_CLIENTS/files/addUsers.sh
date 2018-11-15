f032a7cb68df2d4c28ba1742807dc96405510ed7
Christian Meißner <christian.meissner@unbelievable-machine.com>
2018-11-15 14:26:49 +0100

    add multitenant and applier stuff

---
+++ ansible/roles/MULTIPLE_CLIENTS/files/addUsers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+htpasswd -b /etc/origin/master/htpasswd amy r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd andrew r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd brian r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd betty r3dh4t1!
